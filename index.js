42ca9d0f518f4891ac1bdedd437ea335436a08ad
bewe <16521486@mahasiswa.itb.ac.id>
2023-08-02 19:54:22 +0700

    feat: enable CORS

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const userRouter = require('./api/users/user.router');
 const kegiatanRouter = require('./api/kegiatan/kegiatan.router');
 
 app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
+
 app.use("/api/users", userRouter);
 app.use("/api/kegiatan", kegiatanRouter);
 
 app.listen(process.env.APP_PORT, () => {
-    console.log('Example app listening on port : ', process.env.APP_PORT);
-    }
-);
+  console.log('Example app listening on port : ', process.env.APP_PORT);
+});
