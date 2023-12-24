178d60d6f1747a63998f2d17ebe3475ad2d45c38
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-12-24 18:35:43 +0700

    feat(documentation): api draft

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 
 import { activityRoutes } from "./activity/activity.routes"
+import { documentationRoutes } from "./documentation/documentation.routes"
 
 dotenv.config()
 
@@ -12,6 +13,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10) || 3000
 app.use(cors())
 app.use(express.json())
 app.use("/api/activity", activityRoutes)
+app.use("/api/documentation", documentationRoutes)
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running on port ${PORT}`)
